8ef6492fd15c9ea4b51093599e2c77f80aa5d592
Jonathan Gamble <jdgamble555@gmail.com>
2024-03-04 07:01:44 -0600

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { DataService, DataServiceBrowser } from './data.service';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,7 +21,12 @@ export const appConfig: ApplicationConfig = {
       deps: [DataService],
       useFactory: (ds: DataService) => async () => await ds.load(),
       multi: true
-    }
+    },
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp()),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore())
+    )
   ]
 };
 
